870c729279326ea9f358ff2358859412555791c9
Jonas Hvid <mail@johv.dk>
2020-09-11 18:24:40 +0200

    Don't wrap in the middle of words

--- main.nim
+++ main.nim
@@ -8,10 +8,15 @@ proc extractPrefix(fullLine: string): (string, string) =
 
 proc wrapLine(line: string, prefix: string): string =
     var lines: seq[string] = @[]
-    var segment = 0
-    while segment * 80 < line.len:
-        lines.add(prefix & line.substr(segment * 80, (segment + 1) * 80))
-        segment += 1
+
+    var currentLine: seq[string] = @[]
+    for word in splitWhitespace(line):
+        if prefix.len + currentLine.join(" ").len + word.len > 80:
+            lines.add(prefix & currentLine.join(" "))
+            currentLine = @[]
+        currentLine.add(word)
+    lines.add(prefix & currentLine.join(" "))
+
     lines.join("\n")
 
 let (prefix, line) = extractPrefix(fullLine)
