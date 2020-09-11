466f35974b1eedec1ff166cb34100a84450bd912
Jonas Hvid <mail@johv.dk>
2020-09-11 18:06:38 +0200

    Handle comment characters
    This only handles comments that use a single character followed by a space.

--- main.nim
+++ main.nim
@@ -1,13 +1,18 @@
 import strutils
 
-let line = readLine(stdin)
 
-proc wrapLine(line: string): string =
+let fullLine = readLine(stdin)
+
+proc extractPrefix(fullLine: string): (string, string) =
+    (fullLine.substr(0, 1), fullLine.substr(2))
+
+proc wrapLine(line: string, prefix: string): string =
     var lines: seq[string] = @[]
     var segment = 0
     while segment * 80 < line.len:
-        lines.add(line.substr(segment * 80, (segment + 1) * 80))
+        lines.add(prefix & line.substr(segment * 80, (segment + 1) * 80))
         segment += 1
     lines.join("\n")
 
-echo wrapLine(line)
+let (prefix, line) = extractPrefix(fullLine)
+echo wrapLine(line, prefix)
