7a38701504eff33ffa71a309d5f82da58a83fcbf
Jonas Hvid <mail@johv.dk>
2020-09-11 18:01:40 +0200

    Implement basic line wrapping

--- main.nim
+++ main.nim
@@ -1,5 +1,13 @@
-import strformat
+import strutils
 
 let line = readLine(stdin)
 
-echo fmt"{line}"
+proc wrapLine(line: string): string =
+    var lines: seq[string] = @[]
+    var segment = 0
+    while segment * 80 < line.len:
+        lines.add(line.substr(segment * 80, (segment + 1) * 80))
+        segment += 1
+    lines.join("\n")
+
+echo wrapLine(line)
