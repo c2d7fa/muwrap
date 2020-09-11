8c15468131628b20d83a9c6b15422ae7f48df2a9
Jonas Hvid <mail@johv.dk>
2020-09-11 21:29:08 +0200

    Don't prefix output with comment character when the input isn't a comment
    Closes #3.
    
    We use a basic heuristic for detecting whether something is a comment or not: if
    the first word contains only alpha characters, then it's not a comment; otherwise
    it is.

--- main.nim
+++ main.nim
@@ -1,10 +1,18 @@
-import strutils
+import strutils except splitWhitespace
+import unicode
 
 let fullLine = readLine(stdin)
 
+proc looksLikeCommentPrefix(word: string): bool =
+    not word.isAlpha
+
 proc extractPrefix(fullLine: string): (string, string) =
     let prefix = splitWhitespace(fullLine)[0]
-    (fullLine.substr(0, prefix.len), fullLine.substr(prefix.len))
+    if looksLikeCommentPrefix(prefix):
+        # Assume that there is a space after the prefix.
+        (fullLine.substr(0, prefix.len), fullLine.substr(prefix.len + 1))
+    else:
+        ("", fullLine)
 
 proc wrapLine(line: string, prefix: string): string =
     var lines: seq[string] = @[]
