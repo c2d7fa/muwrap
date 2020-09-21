871b6c9d1f0ab1292fd7ca58b7335308b487b6fb
Jonas Hvid <mail@johv.dk>
2020-09-21 16:13:35 +0200

    Seprate side effects from functionality
    It makes sense to keep the code for reading and writing input separated from the
    rest of the code. This will make unit testing easier.

---
+++ lib.nim
@@ -0,0 +1,31 @@
+import strutils except splitWhitespace
+import unicode
+
+proc looksLikeCommentPrefix(word: string): bool =
+    not word.isAlpha
+
+proc extractPrefix(fullLine: string): (string, string) =
+    let prefix = splitWhitespace(fullLine)[0]
+    if looksLikeCommentPrefix(prefix):
+        # Assume that there is a space after the prefix.
+        (fullLine.substr(0, prefix.len), fullLine.substr(prefix.len + 1))
+    else:
+        ("", fullLine)
+
+proc wrapLine(line: string, prefix: string): string =
+    var lines: seq[string] = @[]
+
+    var currentLine: seq[string] = @[]
+    for word in splitWhitespace(line):
+        if prefix.len + currentLine.join(" ").len + word.len > 80:
+            lines.add(prefix & currentLine.join(" "))
+            currentLine = @[]
+        currentLine.add(word)
+    lines.add(prefix & currentLine.join(" "))
+
+    lines.join("\n")
+
+proc wrap*(text: string): string =
+  let (prefix, line) = extractPrefix(text)
+  wrapLine(line, prefix)
+
--- main.nim
+++ main.nim
@@ -1,31 +1,4 @@
-import strutils except splitWhitespace
-import unicode
+import lib
 
-let fullLine = readLine(stdin)
+echo stdin.readLine.wrap
 
-proc looksLikeCommentPrefix(word: string): bool =
-    not word.isAlpha
-
-proc extractPrefix(fullLine: string): (string, string) =
-    let prefix = splitWhitespace(fullLine)[0]
-    if looksLikeCommentPrefix(prefix):
-        # Assume that there is a space after the prefix.
-        (fullLine.substr(0, prefix.len), fullLine.substr(prefix.len + 1))
-    else:
-        ("", fullLine)
-
-proc wrapLine(line: string, prefix: string): string =
-    var lines: seq[string] = @[]
-
-    var currentLine: seq[string] = @[]
-    for word in splitWhitespace(line):
-        if prefix.len + currentLine.join(" ").len + word.len > 80:
-            lines.add(prefix & currentLine.join(" "))
-            currentLine = @[]
-        currentLine.add(word)
-    lines.add(prefix & currentLine.join(" "))
-
-    lines.join("\n")
-
-let (prefix, line) = extractPrefix(fullLine)
-echo wrapLine(line, prefix)
